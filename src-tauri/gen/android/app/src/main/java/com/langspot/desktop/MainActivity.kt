a5942e0a862677ca572cdfa2b4a1afad61e44899
maylton <maylton.fernandes@gmail.com>
2026-06-14 20:31:01 -0300

    Release LangSpot 1.0.4

---
+++ src-tauri/gen/android/app/src/main/java/com/langspot/desktop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.langspot.desktop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
